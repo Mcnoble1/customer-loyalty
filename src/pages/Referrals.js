cd6ee86b00cd74937826cbb625c2a69d84ce49ac
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-10-18 11:35:34 +0100

    feat: added referrals page :octocat:

---
+++ src/pages/Referrals.js
@@ -0,0 +1,38 @@
+// @mui
+import { Grid, Container, Typography } from '@mui/material';
+// components
+import Page from '../components/Page';
+// sections
+import { AppWidgetSummary } from '../sections/@dashboard/app';
+
+// ----------------------------------------------------------------------
+
+export default function Referra() {
+  return (
+    <Page title="Dashboard">
+      <Container maxWidth="xl">
+        <Typography variant="h4" sx={{ mb: 5 }}>
+          Hi, Welcome back
+        </Typography>
+
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppWidgetSummary title="Weekly Sales" total={714000} icon={'ant-design:android-filled'} />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <AppWidgetSummary title="New Users" total={1352831} color="info" icon={'ant-design:apple-filled'} />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <AppWidgetSummary title="Item Orders" total={1723315} color="warning" icon={'ant-design:windows-filled'} />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <AppWidgetSummary title="Bug Reports" total={234} color="error" icon={'ant-design:bug-filled'} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Page>
+  );
+}
