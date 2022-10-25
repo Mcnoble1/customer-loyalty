52b22e6966931bbcb2ea206f1e09b8f11c08f321
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-10-26 00:13:12 +0100

    feat: implemented API for user enrollment :octocat:

--- src/reach/index.mjs
+++ src/reach/index.mjs
@@ -12,78 +12,51 @@ import * as backend from './build/index.main.mjs';
 
 const stdlib = loadStdlib({ REACH_NO_WARN: 'Y' });
 
-const isAdmin = await ask.ask('Are you an Admin', ask.yesno);
+let done = false;
+const customers = [];
 
-const who = isAdmin ? 'Admin' : 'User';
+// const isAdmin = await ask.ask('Are you an Admin', ask.yesno);
 
-console.log(`Welcome to the Loycus App as ${who}`);
+// const who = isAdmin ? 'Admin' : 'Customer';
 
-const users = [];
+console.log(`Welcome to the Loycus App`);
 
-let acc = null;
-const createAcc = await ask.ask(`Would you like to create an account? (only possible on devnet)`, ask.yesno);
+const accAdmin = await stdlib.newTestAccount(stdlib.parseCurrency(1000));
+// const accCustomer = await stdlib.newTestAccount(stdlib.parseCurrency(1000));
 
-if (createAcc) {
-  acc = await stdlib.newTestAccount(stdlib.parseCurrency(10));
-} else {
-  const secret = await ask.ask('Enter your secret key', (x) => x);
-  acc = await stdlib.newAccountFromSecret(secret);
-}
-
-let ctc = null;
-if (isAdmin) {
-  ctc = acc.contract(backend);
-  ctc.getInfo().then((info) => {
-    console.log(`Your contract is deployed as = ${JSON.stringify(info)}`);
-  });
-} else {
-  const info = await ask.ask('Please paste the contract information: ', JSON.parse);
-  ctc = acc.contract(backend, info);
-  users.push(acc);
-}
-
-const fmt = (x) => stdlib.formatCurrency(x, 4);
-const getBalance = async () => fmt(await stdlib.balanceOf(acc));
+const getBalance = async () => stdlib.formatCurrency(await stdlib.balanceOf(accAdmin)); // get the balance
 
 const before = await getBalance();
 console.log(`Your balance before is ${before} ${stdlib.standardUnit}`);
 
-const balOf = async (acc, tok) => stdlib.balanceOf(acc, tok);
-
-const interact = { ...stdlib.hasRandom };
+// const balOf = async (acc, tok) => stdlib.balanceOf(acc, tok);
 
 let tokenParams = null;
+// let acc = null;
+// let ctc = null;
 
-if (isAdmin) {
-  console.log('Creating the Platform Token');
-  const loyToken = await stdlib.launchToken(acc, 'Loycus', 'LYC', { supply: 500000 }); // Admin creates the NFT
-  console.log(`Launched token:`, loyToken.id.toString());
-  const tokenId = loyToken.id; // NFT id
-  console.log(tokenId);
-  console.log(tokenId.toNumber());
-  // const tokenPar = tokenId.toNumber();
-  tokenParams = tokenId; // params for the NFT
-
-  interact.deadline = { ETH: 100, ALGO: 100, CFX: 1000 }[stdlib.connector];
-}
-
-// if (!isAdmin) {
-//   acc.setDebugLabel('User'); // set the debug label
-//   await acc.tokenAccept(tokenParams); // accept the NFT from the creator
-//   // const getBalance = async () => stdlib.formatCurrency(await stdlib.balanceOf(accUser)); // get the balance
+// if (isAdmin) {
+console.log('Creating the Platform Token');
+const loyToken = await stdlib.launchToken(accAdmin, 'Loycus', 'LYC', { supply: 500000 }); // Admin creates the NFT
+console.log(`Launched token:`, loyToken.id.toString());
+const tokenId = loyToken.id; // NFT id
+console.log(tokenId);
+console.log(tokenId.toNumber());
+// const tokenPar = tokenId.toNumber();
+tokenParams = tokenId; // params for the NFT
+// } else {
+// info = await ask.ask('Please paste the contract information: ', JSON.parse);
+// const ctc = accCustomer.contract(backend, info);
 // }
 
-console.log('Starting backends...');
-
-(interact.getToken = tokenParams),
-  // console.log(`Admin sets the token parameter: `, tokenParams);
-  // const bal = await balOf(acc, tokenParams);
-  // console.log(bal.toString());
-  (interact.enroll = async () => {
-    const firstName = await ask.ask('Enter your First Name', (x) => x);
-    const lastName = await ask.ask('Enter your Last Name', (x) => x);
-    const enrollmentParams = { firstName, lastName };
-    console.log(`You have enrolled into the Loyalty Program.`);
+const startEnrollment = async () => {
+  const newCustomer = async (who) => {
+    const acc = await stdlib.newTestAccount(stdlib.parseCurrency(1000));
+    acc.setDebugLabel(who);
+    customers.push([who, acc]);
+    const info = await ask.ask('Please paste the contract information: ', JSON.parse);
+    const ctc = acc.contract(backend, info);
+    const getBalance = async () => stdlib.formatCurrency(await stdlib.balanceOf(acc));
 
     const after = await getBalance();
     console.log(`Your balance is now ${after}`);
@@ -103,108 +76,210 @@ console.log('Starting backends...');
     // const bal = await balOf(acc, tokenParams);
     // console.log(bal.toString());
 
-    return enrollmentParams;
-  }),
-  (interact.seeUser = async (name, who) => {
-    console.log(`${name} with the address ${stdlib.formatAddress(who)} has enrolled`);
-  }),
-  (interact.referFriend = async () => {
-    const friendName = await ask.ask("Enter your friend's name", (x) => x);
-    // const friendAddress = await ask.ask("Enter your friend's address", (x) => x);
-    const friendParams = { friendName };
-    console.log(`You have referred your friend ${friendName} to the Loyalty Program.`);
-    // console.log(await acc.balanceOf());
-    // console.log(await stdlib.balancesOf(acc, [null, tokenParams]));
+    // return enrollmentParams;
 
-    return friendParams;
-  }),
-  (interact.seeReferral = async (name, who) => {
+    try {
+      await ctc.apis.Customer.enroll();
+      // const firstName = await ask.ask('Enter your First Name', (x) => x);
+      // const lastName = await ask.ask('Enter your Last Name', (x) => x);
+      // const enrollmentParams = { firstName, lastName };
+      console.log(`You have enrolled into the Loyalty Program.`);
+
+      console.log(`You have enrolled into the Loyalty Program`);
+    } catch (e) {
+      console.log(`${who} failed to join the Loyalty Program`);
+    }
+    console.log(`${who} balance after is ${await getBalance()}`);
+
+    const refer = await ask.ask(`Would you like to refer a friend? (y/n)`, ask.yesno);
+    if (refer) {
+      try {
+        await ctc.apis.Customer.referFriend();
+        const friend = await ask.ask(`Enter your friend's name: `, (x) => x);
+        console.log(`You have referred your friend ${friend} to the Loyalty Program.`);
+        return friend;
+      } catch (e) {
+        console.log(`You couldn't refer your friend to the Loyalty Program`);
+      }
+    } else {
+      return null;
+    }
+    console.log(`${who} balance after is ${await getBalance()}`);
+
+    const read = await ask.ask(`Would you like to read the tutorial? (y/n)`, ask.yesno);
+    if (read) {
+      try {
+        await ctc.apis.Customer.readBlog();
+        console.log(`You have read the tutorial.`);
+      } catch (e) {
+        console.log(`You have not read the tutorial.`);
+      }
+    } else {
+      return null;
+    }
+
+    const answer = await ask.ask(`Would you like to answer the question? (y/n)`, ask.yesno);
+    if (answer) {
+      try {
+        await ctc.apis.Customer.answerQuestion();
+        const question = await ask.ask(`Enter your answer: `, (x) => x);
+        const questionParams = { question };
+        console.log(`You have answered the question.`);
+        return questionParams;
+      } catch (e) {
+        console.log(`You have not answered the question.`);
+      }
+    } else {
+      return null;
+    }
+  };
+  await newCustomer('Customer1');
+  await newCustomer('Customer2');
+  await newCustomer('Customer3');
+  while (!done) {
+    await stdlib.wait(0);
+  }
+};
+
+console.log('Starting backends...');
+
+const ctcAdmin = accAdmin.contract(backend);
+ctcAdmin.getInfo().then((info) => {
+  console.log(`Your contract is deployed as = ${JSON.stringify(info)}`);
+  // ctcinfo = info;
+});
+await ctcAdmin.participants.Admin({
+  ...stdlib.hasRandom,
+  getToken: tokenParams,
+
+  // console.log(`Admin sets the token parameter: `, tokenParams);
+  // const bal = await balOf(acc, tokenParams);
+  // console.log(bal.toString());
+
+  ready: () => {
+    console.log('The event is ready to start accepting customers.');
+    startEnrollment();
+  },
+
+  seeCustomer: async (who) => {
+    console.log(`${stdlib.formatAddress(who)} has enrolled`);
+  },
+
+  seeReferral: async (name, who) => {
     console.log(`${name} with the address ${stdlib.formatAddress(who)} referred a friend`);
-  }),
-  (interact.readBlog = async () => {
-    console.log(`You have read the blog post.`);
-  }),
-  (interact.seeRead = async (name, who) => {
+  },
+
+  seeRead: async (name, who) => {
     console.log(`Admin saw that ${name} with the address ${stdlib.formatAddress(who)} has read the blog post`);
-  });
-interact.answerQuestion = async () => {
-  console.log(`You have answered the question.`);
-  return 'Yes';
-};
-interact.seeAnswer = (name, who) => {
-  console.log(`${name} wit the address ${stdlib.formatAddress(who)} has answered the question`);
-};
-interact.seePlay = (name, who) => {
-  console.log(`${name} with the address ${stdlib.formatAddress(who)} has played the game`);
-};
-interact.upgradeToBronze = async () => {
-  const choice = await ask.ask(`Would you like to upgrade to Bronze membership? [costs 250 tokens] (y/n)`, ask.yesno);
-  if (!choice) {
-    console.log(`You are maintaining your membership`);
-  } else {
-    console.log(`You are now a Bronze Member`);
-  }
-};
-interact.seeBronzeUpgrade = (name, who) => {
-  console.log(`${name} with the address ${stdlib.formatAddress(who)} has upgraded to Bronze`);
-};
-interact.upgradeToSilver = async () => {
-  const choice = await ask.ask(`Would you like to upgrade to Silver membership? [costs 500 tokens] (y/n)`, ask.yesno);
-  if (!choice) {
-    console.log(`You are maintaining your membership`);
-  } else {
-    console.log(`You are now a Silver Member`);
-  }
-};
-interact.seeSilverUpgrade = (name, who) => {
-  console.log(`${name} with the address ${stdlib.formatAddress(who)} has upgraded to Silver`);
-};
-interact.upgradeToGold = async () => {
-  const choice = await ask.ask(`Would you like to upgrade to Gold membership? [costs 1000 tokens] (y/n)`, ask.yesno);
-  if (!choice) {
-    console.log(`You are maintaining your membership`);
-  } else {
-    console.log(`You are now a Gold Member`);
-  }
-};
-interact.seeGoldUpgrade = (name, who) => {
-  console.log(`${name} with the address ${stdlib.formatAddress(who)} has upgraded to Gold`);
-};
-interact.upgradeToVIP = async () => {
-  const choice = await ask.ask(`Would you like to upgrade to VIP membership? [costs 2000 tokens] (y/n)`, ask.yesno);
-  if (!choice) {
-    console.log(`You are maintaining your membership`);
-  } else {
-    console.log(`You are now a VIP Member`);
-  }
-};
-interact.seeVipUpgrade = (name, who) => {
-  console.log(`${name} with the address ${stdlib.formatAddress(who)} has upgraded to VIP`);
-};
-interact.playGame = async () => {
-  const choice = await ask.ask(`Would you like to play the game? (y/n)`, ask.yesno);
-  if (choice) {
-    console.log(`You have played the game.`);
-  }
-};
+  },
 
-// implement User's interact object here
+  seePlay: async (name, who) => {
+    console.log(`${name} with the address ${stdlib.formatAddress(who)} has played the game`);
+  },
 
-const part = isAdmin ? ctc.p.Admin : ctc.p.User;
-await part(interact);
+  seeAnswer: async (name, who) => {
+    console.log(`${name} with the address ${stdlib.formatAddress(who)} has answered the question`);
+  },
+
+  seeBronzeUpgrade: async (name, who) => {
+    console.log(`${name} with the address ${stdlib.formatAddress(who)} has upgraded to Bronze`);
+  },
+
+  seeSilverUpgrade: async (name, who) => {
+    console.log(`${name} with the address ${stdlib.formatAddress(who)} has upgraded to Silver`);
+  },
+
+  seeGoldUpgrade: async (name, who) => {
+    console.log(`${name} with the address ${stdlib.formatAddress(who)} has upgraded to Gold`);
+  },
+
+  seeVipUpgrade: (name, who) => {
+    console.log(`${name} with the address ${stdlib.formatAddress(who)} has upgraded to VIP`);
+  },
+});
+
+// interact.upgradeToBronze = async () => {
+//   const choice = await ask.ask(`Would you like to upgrade to Bronze membership? [costs 250 tokens] (y/n)`, ask.yesno);
+//   if (!choice) {
+//     console.log(`You are maintaining your membership`);
+//   } else {
+//     console.log(`You are now a Bronze Member`);
+//   }
+// };
+
+// interact.upgradeToSilver = async () => {
+//   const choice = await ask.ask(`Would you like to upgrade to Silver membership? [costs 500 tokens] (y/n)`, ask.yesno);
+//   if (!choice) {
+//     console.log(`You are maintaining your membership`);
+//   } else {
+//     console.log(`You are now a Silver Member`);
+//   }
+// };
+
+// interact.upgradeToGold = async () => {
+//   const choice = await ask.ask(`Would you like to upgrade to Gold membership? [costs 1000 tokens] (y/n)`, ask.yesno);
+//   if (!choice) {
+//     console.log(`You are maintaining your membership`);
+//   } else {
+//     console.log(`You are now a Gold Member`);
+//   }
+// };
+
+// interact.upgradeToVIP = async () => {
+//   const choice = await ask.ask(`Would you like to upgrade to VIP membership? [costs 2000 tokens] (y/n)`, ask.yesno);
+//   if (!choice) {
+//     console.log(`You are maintaining your membership`);
+//   } else {
+//     console.log(`You are now a VIP Member`);
+//   }
+// };
+
+// interact.playGame = async () => {
+//   const choice = await ask.ask(`Would you like to play the game? (y/n)`, ask.yesno);
+//   if (choice) {
+//     console.log(`You have played the game.`);
+//   }
+// };
 
 // const after = await getBalance();
 const after = await getBalance();
 console.log(`Your balance is now ${after}`);
 
-const balAfter = await balOf(acc, tokenParams);
-console.log(balAfter.toString());
+// const balAfter = await balOf(acc, tokenParams);
+// console.log(balAfter.toString());
 
-for (const account of users) {
+for (const account of customers) {
   // for each bidder
   const [amt, amtToken] = await stdlib.balancesOf(account, [null, tokenParams]); // get the balance
   console.log(`user has ${stdlib.formatCurrency(amt)} ${stdlib.standardUnit} and ${amtToken} of the NFT.`); // log the balance
 }
 
-console.log('Goodbye, Admin and User!');
+console.log('Goodbye, Admin and Customer!');
 ask.done();
+
+done = true;
+
+// const refer = await ask.ask(`Would you like to refer a friend? (y/n)`, ask.yesno);
+//     if (!refer) {
+//       return;
+//     } else {
+//       const friend = await ask.ask(`Enter your friend's name: `, (x) => x);
+//       const friendAcc = await stdlib.newTestAccount(stdlib.parseCurrency(1000));
+//       friendAcc.setDebugLabel();
+//       Customers.push([friend, friendAcc]);
+//       const friendInfo = await ask.ask(
+//         'Please paste the contract information: ',
+//         JSON.parse
+//       );
+//       const friendCtc = friendAcc.contract(backend, friendInfo);
+//       const friendBalance = await getBalance();
+//       console.log(`${friend}'s balance before is ${friendBalance}`);
+//       try {
+//         await friendCtc.apis.Customer.enroll();
+//         console.log(`${friend} has enrolled into the Loyalty Program`);
+//       } catch (e) {
+//         console.log(`${friend} failed to join the Loyalty Program`);
+//       }
+//       console.log(`${friend}'s balance after is ${await getBalance()}`);
+//     }
+//   };
--- src/reach/index.rsh
+++ src/reach/index.rsh
@@ -13,7 +13,7 @@ const platformToken = Token;
 //   lastName: Bytes(64),
 // });
 // let tokenBalance = 0;
-const enrollmentPoints = 10000;
+const enrollmentPoints = 2;
 const referralPoints = 50000;
 const anniversaryPoints = 10;
 const goldMembership = 10000;
@@ -28,12 +28,13 @@ const checkinPoints = 5;
 
 export const main = Reach.App(() => {
   setOptions({ autoTrackPublishedTokens: false });
+  setOptions({ untrustworthyMaps: true });
 
   const Admin = Participant('Admin', {
     // Specify Alice's interact interface here
     getToken: platformToken,
-    createProgram: Fun([], Null),
-    seeUser: Fun([Bytes(64), Address], Null),
+    ready: Fun([], Null),
+    seeCustomer: Fun([Address], Null),
     seeReferral: Fun([Bytes(64), Address], Null),
     seeAnniversary: Fun([Bytes(64), Address], Null),
     seeAnswer: Fun([Bytes(64), Address], Null),
@@ -47,33 +48,34 @@ export const main = Reach.App(() => {
     seeVipUpgrade: Fun([Bytes(64), Address], Null),
     seePurchase: Fun([Bytes(64), Address], Null),
   });
-  const User = Participant('User', {
+  const Customer = API('Customer', {
     // Specify Bob's interact interface here
     enroll: Fun(
       [],
-      Object({
-        firstName: Bytes(64),
-        lastName: Bytes(64),
-      })
+      Null
+      // Object({
+      //   firstName: Bytes(64),
+      //   lastName: Bytes(64),
+      // })
     ),
-    readBlog: Fun([], Null),
-    answerQuestion: Fun([], Bytes(64)),
-    referFriend: Fun(
-      [],
-      Object({
-        friendName: Bytes(64),
-        // account: Address,
-      })
-    ),
-    playGame: Fun([], Null),
-    buyNFT: Fun([], Null),
-    upgradeToGold: Fun([], Null),
-    upgradeToSilver: Fun([], Null),
-    upgradeToBronze: Fun([], Null),
-    upgradeToVIP: Fun([], Null),
-    swapPoints: Fun([UInt], Null),
-    checkin: Fun([], Null),
-    seeAnniversary: Fun([], Null),
+    // readBlog: Fun([], Null),
+    // answerQuestion: Fun([], Bytes(64)),
+    // referFriend: Fun(
+    //   [],
+    //   Object({
+    //     friendName: Bytes(64),
+    //     // account: Address,
+    //   })
+    // ),
+    // playGame: Fun([], Null),
+    // buyNFT: Fun([], Null),
+    // upgradeToGold: Fun([], Null),
+    // upgradeToSilver: Fun([], Null),
+    // upgradeToBronze: Fun([], Null),
+    // upgradeToVIP: Fun([], Null),
+    // swapPoints: Fun([UInt], Null),
+    // checkin: Fun([], Null),
+    // seeAnniversary: Fun([], Null),
   });
   const Info = View('Info', {
     details: platformToken,
@@ -94,122 +96,158 @@ export const main = Reach.App(() => {
   commit();
   Admin.pay([[tokenAmount, token]]); // pay the token into the contract
 
-  assert(balance(token) == tokenAmount, 'balance of token points is wrong');
+  assert(balance(token) == tokenAmount, 'Token points balance is wrong');
+
+  Admin.interact.ready();
+
+  // A set is used for storing addresses
+  const pSet = new Set(); // same
+  // const pMap = new Map(Address, Null);
+
+  const [count] = parallelReduce([0])
+    .invariant(balance(token) == tokenAmount - count * enrollmentPoints && balance() >= 0)
+    .invariant(pSet.Map.size() >= 0)
+    .while(count < 5)
+    .api_(Customer.enroll, () => {
+      // CHECK EXPR -- assumptions about your program
+      check(!pSet.member(this), 'Already enrolled into the loyalty program');
+      check(this != Admin, 'You are the Admin');
+      return [
+        (ret) => {
+          ret(null);
+          pSet.insert(this);
+          const who = this;
+          Admin.interact.seeCustomer(who); // log to the front end
+          transfer(enrollmentPoints, token).to(who);
+          return [count + 1];
+        },
+      ];
+    });
+  // .api_(Customer.readBlog, () => {
+  //   return [ (ret) => {
+  //     ret(null);
+  //     const who = this;
+  //     done = false;
+  //     Admin.interact.confirmGuest(who);
+  //     transfer(enrollmentPoints, token).to(who);
+  //     return [ done ];
+  //   } ];
+  // }),
+  // .api_(Customer.answerQuestion, () => {
 
   // The second one to publish always attaches
 
-  User.only(() => {
-    const { firstName, lastName } = declassify(interact.enroll());
-  });
-  commit();
-  User.publish(firstName, lastName);
+  // Customer.only(() => {
+  //   const { firstName, lastName } = declassify(interact.enroll());
+  // });
+  // commit();
+  // Customer.publish(firstName, lastName);
 
-  Admin.only(() => {
-    interact.seeUser(firstName, User);
-  });
-  transfer(enrollmentPoints, token).to(User);
+  // Admin.only(() => {
+  //   interact.seeCustomer(firstName, Customer);
+  // });
   // token.burn(enrollmentPoints);
 
-  User.only(() => {
-    const { friendName } = declassify(interact.referFriend());
-  });
-  commit();
-  User.publish(friendName);
+  // Customer.only(() => {
+  //   const { friendName } = declassify(interact.referFriend());
+  // });
+  // commit();
+  // Customer.publish(friendName);
 
-  Admin.interact.seeReferral(firstName, User);
+  // Admin.interact.seeReferral(firstName, Customer);
 
-  transfer(referralPoints, token).to(User);
-  // transfer(referralPoints, token).to(ownAccount);
-  // token.burn(2 * referralPoints);
+  // transfer(referralPoints, token).to(Customer);
+  // // transfer(referralPoints, token).to(ownAccount);
+  // // token.burn(2 * referralPoints);
 
-  User.only(() => {
-    interact.readBlog();
-  });
-  commit();
-  User.publish();
+  // Customer.only(() => {
+  //   interact.readBlog();
+  // });
+  // commit();
+  // Customer.publish();
 
-  Admin.interact.seeRead(firstName, User);
+  // Admin.interact.seeRead(firstName, Customer);
 
-  transfer(readTutorial, token).to(User);
-  // token.burn(readTutorial);
+  // transfer(readTutorial, token).to(Customer);
+  // // token.burn(readTutorial);
 
-  User.only(() => {
-    const answer = declassify(interact.answerQuestion());
-  });
-  commit();
-  User.publish(answer);
+  // Customer.only(() => {
+  //   const answer = declassify(interact.answerQuestion());
+  // });
+  // commit();
+  // Customer.publish(answer);
 
-  Admin.interact.seeAnswer(firstName, User);
+  // Admin.interact.seeAnswer(firstName, Customer);
 
-  transfer(answerQuiz, token).to(User);
-  // token.burn(answerQuiz);
+  // transfer(answerQuiz, token).to(Customer);
+  // // token.burn(answerQuiz);
 
-  User.only(() => {
-    interact.playGame();
-  });
-  commit();
-  User.publish();
+  // Customer.only(() => {
+  //   interact.playGame();
+  // });
+  // commit();
+  // Customer.publish();
 
-  Admin.interact.seePlay(firstName, User);
+  // Admin.interact.seePlay(firstName, Customer);
 
-  transfer(playGame, token).to(User);
-  // token.burn(playGame);
+  // transfer(playGame, token).to(Customer);
+  // // token.burn(playGame);
 
-  User.only(() => {
-    const bronze = declassify(interact.upgradeToBronze());
-  });
-  commit();
-  User.pay([[bronzeMembership, token]]);
+  // Customer.only(() => {
+  //   const bronze = declassify(interact.upgradeToBronze());
+  // });
+  // commit();
+  // Customer.pay([[bronzeMembership, token]]);
 
-  Admin.interact.seeBronzeUpgrade(firstName, User);
+  // Admin.interact.seeBronzeUpgrade(firstName, Customer);
 
-  User.only(() => {
-    const silver = declassify(interact.upgradeToSilver());
-  });
-  commit();
-  User.pay([[silverMembership, token]]);
+  // Customer.only(() => {
+  //   const silver = declassify(interact.upgradeToSilver());
+  // });
+  // commit();
+  // Customer.pay([[silverMembership, token]]);
 
-  Admin.interact.seeSilverUpgrade(firstName, User);
+  // Admin.interact.seeSilverUpgrade(firstName, Customer);
 
-  User.only(() => {
-    const gold = declassify(interact.upgradeToGold());
-  });
-  commit();
-  User.pay([[goldMembership, token]]);
+  // Customer.only(() => {
+  //   const gold = declassify(interact.upgradeToGold());
+  // });
+  // commit();
+  // Customer.pay([[goldMembership, token]]);
 
-  Admin.interact.seeGoldUpgrade(firstName, User);
+  // Admin.interact.seeGoldUpgrade(firstName, Customer);
 
-  User.only(() => {
-    const vip = declassify(interact.upgradeToVIP());
-  });
-  commit();
-  User.pay([[vipMembership, token]]);
+  // Customer.only(() => {
+  //   const vip = declassify(interact.upgradeToVIP());
+  // });
+  // commit();
+  // Customer.pay([[vipMembership, token]]);
 
-  Admin.interact.seeVipUpgrade(firstName, User);
+  // Admin.interact.seeVipUpgrade(firstName, Customer);
 
-  // User.only(() => {
+  // Customer.only(() => {
   //   const point = declassify(interact.swapPoints(points));
   // });
   // Admin.publish(points);
 
-  // transfer(points).to(User);
+  // transfer(points).to(Customer);
 
-  // User.only(() => {
+  // Customer.only(() => {
   //   const checkin = declassify(interact.checkin());
   // });
   // commit();
-  // User.publish(checkin);
+  // Customer.publish(checkin);
 
-  // transfer(checkinPoints, token).to(User);
+  // transfer(checkinPoints, token).to(Customer);
   // token.burn(checkinPoints);
 
-  // User.only(() => {
+  // Customer.only(() => {
   //   const anniversary = declassify(interact.seeAnniversary());
   // });
   // commit();
-  // User.publish(anniversary);
+  // Customer.publish(anniversary);
 
-  // transfer(anniversaryPoints, token).to(User);
+  // transfer(anniversaryPoints, token).to(Customer);
   // token.burn(anniversaryPoints);
 
   transfer(balance(token), token).to(Admin);
@@ -222,7 +260,7 @@ export const main = Reach.App(() => {
 
 // Pseudocode for Loyalty DApp
 
-//  Users
+//  Customers
 // Connect Wallet
 // - Get wallet address
 // - keep track of Wallet balance
@@ -235,45 +273,45 @@ export const main = Reach.App(() => {
 
 // Referral System
 // - Get a Friend to enroll and receive referral bonus
-// - → Retrieve their Username → Send Friend 5 points on new enrollment
+// - → Retrieve their Customername → Send Friend 5 points on new enrollment
 // - Receive 5 points as well
 // - Notify user and Admin about referral and points using reach views
 
 // Games
 //  RPS, Morra, Fortune Teling, etc.
-// - User gets rewarded 1point for playing a game
-// - User gets rewarded 5points if they win the game
-// - User gets no point if they lose the game
+// - Customer gets rewarded 1point for playing a game
+// - Customer gets rewarded 5points if they win the game
+// - Customer gets no point if they lose the game
 
 // Blogs/Tutorials
-// - Users earn tokens when they read web3 blogs/take tutorials and answer questions on our platform
-// - User gets rewarded 1 point for reading a blog
-// - User gets rewarded 5 points for answering a quiz correctly
+// - Customers earn tokens when they read web3 blogs/take tutorials and answer questions on our platform
+// - Customer gets rewarded 1 point for reading a blog
+// - Customer gets rewarded 5 points for answering a quiz correctly
 //  Notify user and Admin about points using reach views
 
 // Checkins and Anniversaries
-// - Users receive tokens on anniversaries, say when some length in block time has elapsed
-// - Users receive tokens each time they sign in into the platform
+// - Customers receive tokens on anniversaries, say when some length in block time has elapsed
+// - Customers receive tokens each time they sign in into the platform
 //  Notify user and Admin about points using reach views
 
 // Token/Points redemption
 // 1) Membership Levels
-//   Users can use the points or token accumulated to buy higher membership levels.
+//   Customers can use the points or token accumulated to buy higher membership levels.
 //  VIP: 1000 points
 //  Gold: 500 points
 //  Silver: 100 points
 //  Bronze: 10 points
 
 // 2) Store/NFT marektplace
-// - Users can buy NFTs/other products with their points
-// - Users can sell NFTs for points
+// - Customers can buy NFTs/other products with their points
+// - Customers can sell NFTs for points
 
 // 3) Swap Points for Network Tokens	(if posssible)
-// - Users can swap their points for Testnet ALGOs
+// - Customers can swap their points for Testnet ALGOs
 // - Withdraw from Contract
 // - Award 5points on successful Withdrawal
 
 //  Admin
-// - See User's Information
+// - See Customer's Information
 // - View user's point balance
-// - View User's events and activities
+// - View Customer's events and activities
